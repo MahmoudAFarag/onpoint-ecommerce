482c06bd0beb3d92226c8bcba27d90a689472820
Mahmoud Ashraf <mahmoudashrafsaad8@gmail.com>
2022-04-06 18:14:59 +0200

    migrate to tailwind grid view and add pagination

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -1,20 +1,38 @@
-import styles from '../styles/HomePage.module.css';
 import React, { useEffect, useState } from 'react';
 import { getProducts } from '../lib/products';
 import ProductCard from '../components/ProductCard';
+import Pagination from '../components/Pagination';
 
 const HomePage = () => {
   const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const paginatedProducts = products.slice((currentPage - 1) * 6, currentPage * 6);
 
   useEffect(() => {
     getProducts().then((data) => setProducts(data));
   }, []);
 
-  const renderedContent = products.map((product) => {
-    return <ProductCard key={product.id} product={product} />;
-  });
+  if (products.length === 0) {
+    return <h1>Loading</h1>;
+  }
 
-  return <div className={styles.home__page}>{renderedContent}</div>;
+  const handlePageChange = (e, page) => {
+    e.preventDefault();
+
+    setCurrentPage(page);
+  };
+
+  return (
+    <>
+      <div className='grid justify-items-center gap-8 p-8 md:grid-cols-2 lg:grid-cols-3'>
+        {paginatedProducts.map((product) => {
+          return <ProductCard key={product.id} product={product} />;
+        })}
+      </div>
+      <Pagination itemsCount={products.length} pageSize={6} currentPage={currentPage} onPageChange={handlePageChange} />
+    </>
+  );
 };
 
 export default HomePage;
