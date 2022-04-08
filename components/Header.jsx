f90b9f2110a0e4cbccfcab263b4f533db2c50c1d
Mahmoud Ashraf <mahmoudashrafsaad8@gmail.com>
2022-04-08 14:05:06 +0200

    adjust header and footer

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,46 @@
+import React from 'react';
+
+import { FaSearch, FaShoppingCart, FaBars, FaCircle } from 'react-icons/fa';
 import styles from '../styles/Header.module.css';
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <h1 className={styles.header__text}>Header components</h1>
+      <a href='#' className={styles.logo}>
+        <FaCircle className={styles.circle} />
+        <span>N Point</span>
+      </a>
+
+      <form action='' className={styles.search_form}>
+        <label htmlFor='search-box'>
+          <FaSearch />
+        </label>
+        <input type='text' placeholder='search here' id='search-box' />
+        <label htmlFor='search-box'>
+          <input type='submit' value='search' className={styles.btn} />
+        </label>
+      </form>
+      <nav className={styles.nav}>
+        <a href='#'>Sign In</a>
+        <a href='#'>Sign Up</a>
+      </nav>
+      <div className={styles.sign_dev}>
+        <a href='#' className={styles.sign}>
+          Sign In
+        </a>
+        <a href='#' className={styles.sign}>
+          Sign Up
+        </a>
+      </div>
+      <div className={styles.icons}>
+        <div id={styles.menu_btn}>
+          <FaBars />
+        </div>
+        <div id={styles.cart_btn}>
+          <FaShoppingCart />
+        </div>
+      </div>
     </header>
   );
 };
-
 export default Header;
