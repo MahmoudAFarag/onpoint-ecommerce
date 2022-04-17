2b8ab6e16d642fa4f602f0407f38ddd6b3ce7ec8
Mahmoud Ashraf <mahmoudashrafsaad8@gmail.com>
2022-04-17 17:10:14 +0200

    handle cart adding

--- components/cart/AddToCartButton.jsx
+++ components/cart/AddToCartButton.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import useStore from '../../store/useStore';
-// import useCartStore from '../../store/CartStore';
 import styles from '../../styles/ShoppingCart.module.css';
 
 const AddToCartButton = ({ product }) => {
-  // const addItemToCart = useCartStore((state) => state.addItemToCart);
   const addItemToCart = useStore((state) => state.addItem);
-
 
   return (
     <React.Fragment>
-      <button
-        onClick={() => addItemToCart(product)}
-        className={styles.add_button}
-      >
+      <button onClick={() => addItemToCart(product)} className={styles.add_button}>
         Add To Cart
       </button>
     </React.Fragment>
--- store/CartStore.jsx
+++
@@ -1,9 +0,0 @@
-import create from 'zustand';
-
-const useCartStore = create((set) => ({
-  cartItems: [],
-  addItemToCart: (item) => set((state) => ({ cartItems: [...state.cartItems, item] })),
-  removeAllCartItems: () => set({ cartItems: [] }),
-}));
-
-export default useCartStore;
