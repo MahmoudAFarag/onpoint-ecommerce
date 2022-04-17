11b747559354d109ddd90b5b15a66bd689593152
AMIREZZAT <amirame3360@gmail.com>
2022-04-17 16:15:54 +0200

    Adding daynamic route ,more details

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const ProductCard = ({ product }) => {
   return (
     
-    <Link href={{pathname:'/unauthsingleproduct', query:{object:JSON.stringify(product)}}}>
+    
     <div className='mx-2 max-w-sm overflow-hidden ' >
       <Image src={product.image} alt='product image' width={400} height={300} />
       <div className='px-3'>
@@ -13,12 +13,12 @@ const ProductCard = ({ product }) => {
       </div>
       <div className='mb-3 flex items-center p-3'>
         <h3 className='text-md mr-auto'>${product.price}</h3>
-        <Link href={product.id}>
-          <a className='bg-amber-400 px-3 py-2'>Buy now</a>
+        <Link href={`/product/${product.id}`}>
+          <a className='bg-amber-400 px-3 py-2'>More Details</a>
         </Link>
       </div>
     </div>
-    </Link>
+    
   );
 };
 
