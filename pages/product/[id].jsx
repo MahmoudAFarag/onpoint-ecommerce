1be0c43b077ca1ed2182bb5f2346e01057e55edb
AMIREZZAT <amirame3360@gmail.com>
2022-04-22 01:20:56 +0200

    maping the review

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -1,246 +1,124 @@
-// import React, { useEffect, useState } from "react"
-// import styles from '../../styles/SingleProduct.module.css';
-// import Header from "../../components/Header";
-// import Footer from '../../components/Footer';
-// import { useRouter } from "next/router";
-// import Link from 'next/link';
-// import { getProduct } from "../../lib/products";
-// import { async } from "@firebase/util";
-
-
-
-// const Product =  () => {
-      
-// const [product,setProduct]=useState({})
-    
-      
-        
-  
-    
-
-     
-
-//         const router = useRouter();
-
-       
-//           const {id} = router.query
-          
-//            console.log(id)
-     
-         
-         
-        
-          
-         
-          
-//        useEffect(()=>{
-//         getProduct(id).then(data=>{
-//           console.log(data)
-//           setProduct(data)
-//       }).catch(err=>{
-//           console.log(err)
-//       })
-//       console.log(id)
-    
-//        },[])
-//        console.log(product)
-          
-      
-        
-       
-      
-
-      
-
-        
-
-
-      
-       
-      
-         
-    
-//     return (
-//   <>
-   
-//          <Header/>
-
-//               <div className={styles.singleproduct}>                               
-//                                   <div className={styles.singleproduct_info} >
-//                                         <img src={product.image} className={styles.avatar_product} />
-//                                           <div>
-//                                                   <div>
-                                                    
-//                                                      <strong className={styles.singleproduct_info_text}></strong>
-//                                                   </div>
-//                                                   <div>
-//                                                      <span className={styles.singleproduct_info_text}>Brand: {product.category}</span>
-//                                                   </div>
-//                                                      <span className={styles.singleproduct_info_text}>Price:${product.price}</span>
-//                                                   <div>
-//                                                                           <br></br>
-//                                                   <div>
-//                                                      <strong className={styles.singleproduct_info_text}>About This Item</strong>
-//                                                  </div>
-//                                                      <span className={styles.singleproduct_info_text}>{product.description}</span>
-//                                                  </div>
-//                                                                           <br></br>
-//                                               <div className={styles.buttontwo}>
-//                                                    <Link href={{pathname:'/unauthcashout'}}>
-//                                                      <button className={styles.button} > Buy Now </button>
-//                                                      </Link>
-//                                                      <button className={styles.button1} > Add To Cart</button>
-//                                               </div>
-//                                           </div>
-//                                   </div> 
-
-//                   <div>
-//                             <div>
-//                            <strong className={styles.singleproduct_info_text}><h3>Reviews</h3></strong>
-//                             </div>
-//                             <div>
-//                                 <div>
-//                                   <strong className={styles.singleproduct_info_text}>Steve R</strong>
-//                                 </div> 
-//                                   <span className={styles.singleproduct_info_text}>I’ll start with what I dislike, it’s a real shame, but video cards are way overpriced! It’s a great card </span>
-//                           </div>
-//                                                    <br></br>
-//                           <div>
-//                               <div>
-//                                 <strong className={styles.singleproduct_info_text}>Marie J</strong>
-//                               </div> 
-//                                 <span className={styles.singleproduct_info_text}>I’ll start with what I dislike, it’s a real shame, but video cards are way overpriced! It’s a great card </span>
-//                          </div>
-//                     <div>
-//                            <label  htmlFor=" text" className={styles.singleproduct_info_text}><h4><strong>TELL US WHAT YOU THINK</strong></h4></label>
-//                     </div>
-
-//                  <div>
-                 
-                                    
-//                                  <div>
-//                                      <h2>You Need To <span className={styles.tex}>Sign In</span> To Tell Us What You Think! </h2>
-//                                  </div>   
-//                  </div>
-
-//                  </div>
-                                       
-                                                   
-                                       
-//               </div>
-//          <Footer/>    
-// </>        
-         
-//     );
-//   };
-//   export default Product;
-
-import React from 'react';
+import React, { useEffect, useState } from "react"
 import styles from '../../styles/SingleProduct.module.css';
+import Header from "../../components/Header";
 import Footer from '../../components/Footer';
-import { withRouter } from 'next/router';
+import { useRouter } from "next/router";
 import Link from 'next/link';
+import { getProduct } from "../../lib/products";
 import useStore from '../../store/useStore';
 import AddToCartButton from '../../components/cart/AddToCartButton';
-import { shimmer, toBase64 } from '../../lib/image_placeholder';
-import Image from 'next/image';
-
-const Product = ({ router: { query } }) => {
-  
-  const addItem = useStore((state) => state.addItem)
-  ;
-
-  const product = JSON.parse(query.object);
 
 
+
+const Product =  () => {
+      
+const [product,setProduct]=useState([])
+const addItem = useStore((state) => state.addItem)
+      
+
+        const router = useRouter();
  
-   
+          const {id} = router.query
+          
+           console.log(id)
+  
+          
+       useEffect(()=>{
+        getProduct(id).then(product=>{
+          console.log(product)
+          setProduct(product)
+      })
+      console.log(id)
+    
+       },[])
+       console.log(product)
+          
+      
+    
   const handleAddCart = () => {
     addItem(product);
 
   };
-
-  return (
-    <div>
-      <div className={styles.singleproduct}>
-        <div className={styles.singleproduct_info}>
-          <Image
-            src={product.image}
-            alt='product image'
-            width={250}
-            height={250}
-            placeholder='blur'
-            blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(250, 250))}`}
-          />
-          <div>
-            <div>
-              <strong className={styles.singleproduct_info_text}>{product.name}</strong>
-            </div>
-            <div>
-              <span className={styles.singleproduct_info_text}>Brand: {product.category}</span>
-            </div>
-            <span className={styles.singleproduct_info_text}>Price:${product.price}</span>
-            <div>
-              <br></br>
-              <div>
-                <strong className={styles.singleproduct_info_text}>About This Item</strong>
+        
+   
+    
+    return (
+      <div>
+          <div className={styles.singleproduct}>
+             <div className={styles.singleproduct_info}>
+               <img src={product.image} className={styles.avatar_product} />
+                <div>
+                  <div>
+                    <strong className={styles.singleproduct_info_text}>{product.name}</strong>
+                  </div>
+                  <div>
+                    <span className={styles.singleproduct_info_text}>Brand: {product.category}</span>
+                  </div>
+                  <span className={styles.singleproduct_info_text}>Price:${product.price}</span>
+                  <div>
+                    <br></br>
+                    <div>
+                      <strong className={styles.singleproduct_info_text}>About This Item</strong>
+                    </div>
+                    <span className={styles.singleproduct_info_text}>{product.description}</span>
+                  </div>
+                  <br></br>
+                  <div className={styles.buttontwo}>
+                    <Link href={{ pathname: '/unauthcashout' }}>
+                      <button className={styles.button}> Buy Now</button>
+                    </Link>
+                    <AddToCartButton product={product} />
+                  </div>
+                </div>
               </div>
-              <span className={styles.singleproduct_info_text}>{product.description}</span>
+      
+              <div>
+                <div>
+                  <strong className={styles.singleproduct_info_text}>
+                    <h3>Reviews</h3>
+                  </strong>
+                </div>
+                <div>
+                  <ul>
+                 {product.reviews?.map((review)=>(
+                   <li key={ review.name}>
+          
+                 <div>
+                    <strong className={styles.singleproduct_info_text}>{review.name}</strong>
+                  </div>
+                  <span className={styles.singleproduct_info_text}>
+                   {review.review}
+                  </span>
+          
+                </li>
+                    ))}
+                    </ul>
+                 
+                </div>
+            
+                <br></br>
+             
+                <div>
+                  <label htmlFor=' text' className={styles.singleproduct_info_text}>
+                    <h4>
+                      <strong>TELL US WHAT YOU THINK</strong>
+                    </h4>
+                  </label>
+                </div>
+      
+                <div>
+                  <div>
+                    <h2>
+                      You Need To <span className={styles.tex}>Sign In</span> To Tell Us What You Think!{' '}
+                    </h2>
+                  </div>
+                </div>
+              </div>
             </div>
-            <br></br>
-            <div className={styles.buttontwo}>
-              <Link href={{ pathname: '/unauthcashout' }}>
-                <button className={styles.button}> Buy Now</button>
-              </Link>
-              <AddToCartButton product={product} />
-            </div>
+            <Footer />
           </div>
-        </div>
-
-        <div>
-          <div>
-            <strong className={styles.singleproduct_info_text}>
-              <h3>Reviews</h3>
-            </strong>
-          </div>
-          <div>
-            <div>
-              <strong className={styles.singleproduct_info_text}>Steve R</strong>
-            </div>
-            <span className={styles.singleproduct_info_text}>
-              I’ll start with what I dislike, it’s a real shame, but video cards are way overpriced! It’s a great card{' '}
-            </span>
-          </div>
-          <br></br>
-          <div>
-            <div>
-              <strong className={styles.singleproduct_info_text}>Marie J</strong>
-            </div>
-            <span className={styles.singleproduct_info_text}>
-              I’ll start with what I dislike, it’s a real shame, but video cards are way overpriced! It’s a great card{' '}
-            </span>
-          </div>
-          <div>
-            <label htmlFor=' text' className={styles.singleproduct_info_text}>
-              <h4>
-                <strong>TELL US WHAT YOU THINK</strong>
-              </h4>
-            </label>
-          </div>
-
-          <div>
-            <div>
-              <h2>
-                You Need To <span className={styles.tex}>Sign In</span> To Tell Us What You Think!{' '}
-              </h2>
-            </div>
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </div>
-  );
-};
-export default withRouter(Product);
-
-
+        );
+      };
+      export default Product;
+      
+   
