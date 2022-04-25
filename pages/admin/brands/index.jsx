9f66eb3d75d4f355e2fe995de10d6e78da187399
Mostafa Ezzat <psmostafa@gmail.com>
2022-04-26 01:59:47 +0200

    create brands store/slice

--- pages/admin/brands/index.jsx
+++ pages/admin/brands/index.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+// Components
+import Add from "../../../components/categories/Add";
+import List from "../../../components/categories/List";
+import SimpleMessage from "../../../components/SimpleMessage";
+
+// Firebase
+import checkUserIsAdmin from "../../../lib/checkUserIsAdmin";
+import brandsSnapshot from "../../../lib/brandsSnapshot";
+
+// State Management
+import useStore from "../../../store/useStore";
+import useCreateBrandSlice from "../../../store/useCreateBrandSlice";
 
 const Index = () => {
+  const auth = useStore((state) => state.currentUser);
+  const brandsStore = useCreateBrandSlice();
+  const [userMessage, setUserMessage] = useState({ show: false, message: "" });
+  const router = useRouter();
   return <div>Index</div>;
 };
 
 export default Index;
+
+// Todo
+// 1- create brands snapshot
+// 2- create brands store
---
+++ store/createBrandsSlice.js
@@ -0,0 +1,61 @@
+import create from "zustand";
+
+const initalState = {
+  loading: true,
+  brands: [],
+};
+
+const useCreateBrandsSlice = create((set) => ({
+  value: initalState,
+
+  add: (brand) =>
+    set((state) => {
+      const isExist = state.value.brands.findIndex(
+        (item) => item.id == brand.id
+      );
+
+      if (isExist === -1) {
+        return {
+          value: {
+            ...state.value,
+            brands: [...state.value.categories, brand],
+          },
+        };
+      }
+    }),
+
+  update: (brand) =>
+    set((state) => {
+      const isExist = state.value.brands.findIndex(
+        (item) => item.id == brand.id
+      );
+
+      if (isExist !== -1) {
+        const newValue = [...state.value.brands];
+        newValue[isExist] = brand;
+
+        return { value: { ...state.value, brands: newValue } };
+      }
+    }),
+
+  delete: (brand) =>
+    set((state) => {
+      const isExist = state.value.brands.findIndex(
+        (item) => item.id == brand.id
+      );
+
+      if (isExist !== -1) {
+        const newValue = state.value.brands.filter(
+          (item) => item.id != category.id
+        );
+
+        return { value: { ...state.value, brands: newValue } };
+      }
+    }),
+
+  finishLoading: () => {
+    set((state) => ({ value: { ...state.value, loading: false } }));
+  },
+}));
+
+export default useCreateBrandsSlice;
