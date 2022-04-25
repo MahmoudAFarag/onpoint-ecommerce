8175077c4004450742b96d9f4300581c8a16976d
AMIREZZAT <amirame3360@gmail.com>
2022-04-25 16:17:38 +0200

    rout for purchase confirmation

---
+++ pages/thanksforpurchase.jsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import ThanksForPurchase from '../components/Forms/ThanksForPurchase';
+
+const ThankYouForPurchase =()=>{
+
+    return(
+        <ThanksForPurchase/>
+          )
+}
+export default ThankYouForPurchase;
