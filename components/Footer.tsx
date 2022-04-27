f6a452640d48996214f33220d79eb2a9537b8ceb
Mahmoud Ashraf <mahmoudashrafsaad8@gmail.com>
2022-04-28 01:23:29 +0200

    remove old footer code

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -58,44 +58,6 @@ const Footer = () => {
         </div>
       </div>
     </footer>
-    // <footer className={styles.footer}>
-    //   <div className={styles.pay_methode}>
-    //     <a href="#" id={styles.Visa}>
-    //       <FaCcVisa />
-    //     </a>
-    //     <a href="#" id={styles.mastercard}>
-    //       <FaCcMastercard />
-    //     </a>
-    //     <a href="#" id={styles.credit_card}>
-    //       <FaCreditCard />
-    //     </a>
-    //   </div>
-    //   <div className={styles.Contact}>
-    //     <span>Follow Us</span>
-    //     <div className={styles.social}>
-    //       <a href="#" className={styles.fb}>
-    //         <FaFacebookF />
-    //       </a>
-    //       <a href="#" className={styles.tw}>
-    //         <FaTwitter />
-    //       </a>
-    //       <a href="#" className={styles.ln}>
-    //         <FaLinkedin />
-    //       </a>
-    //       <a href="#" className={styles.ins}>
-    //         <FaInstagram />
-    //       </a>
-    //     </div>
-    //     <div className={styles.contact_btns}>
-    //       <a href="#Gmail.com" className={styles.env}>
-    //         Mail Us
-    //       </a>
-    //       <Link href="/ContactUs">
-    //         <a>Contact Us</a>
-    //       </Link>
-    //     </div>
-    //   </div>
-    // </footer>
   );
 };
 
