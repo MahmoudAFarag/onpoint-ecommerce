de1f35567b1070713b6a8a78303368db8eacef00
Mahmoud Ashraf <mahmoudashrafsaad8@gmail.com>
2022-04-29 22:42:18 +0200

    finish shopping cart responsive page

--- components/cart/ShoppingCart.tsx
+++ components/cart/ShoppingCart.tsx
@@ -1,9 +1,3 @@
-// Core
-import React from 'react';
-
-// Style
-import styles from '../../styles/ShoppingCart.module.css';
-
 // Stores
 import useStore from '../../store/useStore';
 
@@ -17,14 +11,37 @@ const ShoppingCart = () => {
   const cartItems = useStore((state) => state.items);
   const hasHydrated = useHasHydrated();
 
+  // calculations
+  const subtotal = +cartItems
+    .reduce((acc, item) => {
+      return acc + item.price * item.cartQuantity;
+    }, 0)
+    .toFixed(1);
+
+  const taxEstimate = +(subtotal * 0.05).toFixed(1);
+
+  const shippingEstimate = 10;
+
+  const total = subtotal + taxEstimate + shippingEstimate;
+
   if (!hasHydrated) {
     return <Spinner />;
   }
 
+  if (cartItems.length === 0) {
+    return (
+      <div className='flex h-[50vh] flex-col items-center justify-center gap-3 text-xl md:gap-5 md:text-4xl'>
+        <h3 className='text-gray-500'>Your cart is empty</h3>
+        <Link href='/'>
+          <a className='text-base text-gray-500 underline underline-offset-4 md:text-lg'>Continue shopping</a>
+        </Link>
+      </div>
+    );
+  }
+
   return (
-    <div className='p-5'>
-      <h1 className='mb-4 text-xl'>ShoppingCart</h1>
-      {/* <div>{cartItems.length === 0 && <h4 className={styles.header__no_items}>No Items in Cart</h4>}</div> */}
+    <div className='p-5 md:p-10'>
+      <h1 className='mb-4 text-xl md:text-3xl'>ShoppingCart</h1>
 
       <div className='flex flex-col gap-3'>
         {cartItems.map((cartItem) => (
@@ -32,13 +49,38 @@ const ShoppingCart = () => {
         ))}
       </div>
 
-      {cartItems.length !== 0 && (
-        <div className='my-5 mx-auto w-max'>
-          <Link href='/cashout' passHref>
-            <button className='rounded bg-amber-400 py-2 px-4  pb-3 text-xs font-bold uppercase '> Proceed to CheckOut</button>
-          </Link>
+      <div className='mx-auto mt-4 flex w-full flex-col p-4 text-base shadow-md md:w-[85%] md:text-2xl'>
+        <h1 className='mb-4'>Order Summary</h1>
+        <div className='flex flex-col gap-3 text-xs md:text-base'>
+          <div className='flex border-b-2 pb-2'>
+            <p className='mr-auto'>Subtotal</p>
+            <p>$ {subtotal}</p>
+          </div>
+
+          <div className='flex border-b-2 pb-2'>
+            <p className='mr-auto'>Tax Estimate</p>
+            <p>$ {taxEstimate}</p>
+          </div>
+
+          <div className='flex border-b-2 pb-2'>
+            <p className='mr-auto'>Shipping Estimate</p>
+            <p>$ {shippingEstimate}</p>
+          </div>
+
+          <div className='md: flex text-base md:text-xl'>
+            <p className='mr-auto'>Order Total</p>
+            <p>$ {total}</p>
+          </div>
+
+          <div className='my-2 self-center'>
+            <Link href='/cashout' passHref>
+              <button className='rounded bg-amber-400 py-2 px-4 text-xs font-bold uppercase md:py-3 md:px-5 md:text-lg'>
+                Proceed to CheckOut
+              </button>
+            </Link>
+          </div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
